7c230465291753ebe490fe78f87b7959a37963a1
Otti Daniel <33727291+Sir-Dazzling@users.noreply.github.com>
2020-02-17 08:46:26 +0100

    added new Components

--- src/com/denvato_v2/config/DenvatoSecurityConfig.java
+++ src/com/denvato_v2/config/DenvatoSecurityConfig.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
